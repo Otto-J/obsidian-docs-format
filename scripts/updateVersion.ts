da7f280e122f8c37defe0608133c79b634fd3dad
Otto <fa@ijust.cc>
2023-10-26 11:01:00 +0800

    feat: 更新基础脚本

--- scripts/updateVersion.ts
+++ scripts/updateVersion.ts
@@ -1,7 +1,26 @@
-// import readline from "readline";
 import fsPromise from "fs/promises";
-// import { exec } from "child_process";
+import { exec } from "child_process";
 import inquirer from "inquirer";
+import pkg from "../package.json";
+import manifest from "../manifest.json";
+
+async function syncManifest() {
+  const info = {
+    version: pkg.version,
+    description: pkg.description,
+    author: pkg.author,
+  };
+
+  const newManifest = {
+    ...manifest,
+    ...info,
+  };
+
+  fsPromise.writeFile(
+    "./manifest.json",
+    JSON.stringify(newManifest, null, 2) + "\n"
+  );
+}
 
 async function main() {
   const data = await fsPromise.readFile("./package.json", "utf8");
@@ -41,6 +60,37 @@ async function main() {
     "utf8"
   );
   console.log(`版本号已更新为: ${pkg.version}`);
+  // 请问是否同步更新 manifest.json 的版本号
+  const { isSync } = await inquirer.prompt([
+    {
+      type: "confirm",
+      name: "isSync",
+      message: "请问是否同步更新 manifest.json 的版本号",
+    },
+  ]);
+  if (isSync) {
+    await syncManifest();
+    console.log("manifest.json 已更新");
+  }
+  // 请问是否需要发布新的 git tag
+  const { isTag } = await inquirer.prompt([
+    {
+      type: "confirm",
+      name: "isTag",
+      message: "请问是否需要发布新的 git tag",
+    },
+  ]);
+  if (isTag) {
+    exec(`git tag v${pkg.version}`, (err, stdout, stderr) => {
+      if (err) {
+        console.log(err);
+        process.exit(1);
+      }
+      console.log(stdout);
+      console.log(stderr);
+    });
+    console.log(`git tag v${pkg.version}`);
+  }
 }
 
 main();
